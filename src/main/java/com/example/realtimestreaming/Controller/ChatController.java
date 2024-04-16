481bcbd022558e5e348b19a519462941c4fc2e59
unknown <sola2014@naver.com>
2024-04-17 00:03:23 +0900

    fix: Controller 에러 수정

--- src/main/java/com/example/realtimestreaming/Controller/ChatController.java
+++ src/main/java/com/example/realtimestreaming/Controller/ChatController.java
@@ -1,28 +1,11 @@
 package com.example.realtimestreaming.Controller;
 
-import com.example.realtimestreaming.Common.HTTP_INTERNAL_SERVER_ERROR;
-import com.example.realtimestreaming.Dto.Request.Stream.SendChatReq;
-import com.example.realtimestreaming.Service.StreamService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/chat")
-public class StreamController {
+public class ChatController {
     @GetMapping("/info")
     public String handleInfoRequest(@RequestParam("t") String timestamp) {
         // t 매개변수를 사용하여 필요한 처리를 수행
