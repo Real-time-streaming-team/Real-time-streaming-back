bd66be015bbd5b3dd571d29b2aa266ee1705e17d
unknown <sola2014@naver.com>
2024-04-17 15:55:32 +0900

    chore: cors 설정 변경

---
+++ src/main/java/com/example/realtimestreaming/Config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.example.realtimestreaming.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        // 허용할 출처 설정
+        config.addAllowedOrigin("*");
+        // 허용할 HTTP 메서드 설정
+        config.addAllowedMethod("*");
+        // 허용할 헤더 설정
+        config.addAllowedHeader("*");
+        // 인증 정보 허용 설정
+        config.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
