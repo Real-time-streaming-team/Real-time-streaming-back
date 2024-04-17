ca9ec689962ca45e03c5cfde1542f17cd89d5b8f
unknown <sola2014@naver.com>
2024-04-17 15:37:59 +0900

    chore: cor 설정 변경

--- src/main/java/com/example/realtimestreaming/Config/WebConfig.java
+++ src/main/java/com/example/realtimestreaming/Config/WebConfig.java
@@ -1,22 +1,40 @@
 package com.example.realtimestreaming.Config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
-import java.util.Collections;
+@SpringBootConfiguration
+public class WebConfig {
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public CorsFilter corsFilter() {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
+        //CorsConfiguration 개체 생성 후 설정 추가
+        CorsConfiguration config = new CorsConfiguration();
+        //내보낼 원본 도메인 설정
+        config.addAllowedOrigin("*");
+        //원본 요청 헤더 정보 내보내기
+        config.addAllowedHeader("*");
+        //header 의 노출 정보
+        config.addExposedHeader("*");
+        //허용할 요청 항목들
+        config.addAllowedMethod("GET");     //get
+        config.addAllowedMethod("PUT");     //put
+        config.addAllowedMethod("POST");    //post
+        config.addAllowedMethod("DELETE");  //delete
+        //corsConfig.addAllowedMethod("*");     //모두허용
 
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowCredentials(true)
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .maxAge(3600);
+        // Cookie 전송여부
+        config.setAllowCredentials(true);
+
+        //2. 매핑 경로 추가
+        UrlBasedCorsConfigurationSource corsConfigurationSource =
+                new UrlBasedCorsConfigurationSource();
+        corsConfigurationSource.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(corsConfigurationSource);
     }
 }
