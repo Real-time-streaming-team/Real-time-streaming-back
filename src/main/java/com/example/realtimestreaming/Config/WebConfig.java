bffd6816e860f069e15a0e5494cb9a7cc287cb38
unknown <sola2014@naver.com>
2024-04-17 13:47:40 +0900

    chore: cors 설정 변경

--- src/main/java/com/example/realtimestreaming/Config/WebConfig.java
+++ src/main/java/com/example/realtimestreaming/Config/WebConfig.java
@@ -4,16 +4,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Collections;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+
+        //  Set routes that allow cross domain routing
         registry.addMapping("/**")
-                .allowedOriginPatterns("*") // 모든 출처 허용
-                .allowedMethods("*")
-                .allowedHeaders("*")
+                //  Set the domain name that allows cross domain requests
+                //.allowedOrigins("*")
+                // Cross domain configuration error , take .allowedOrigins Replace with .allowedOriginPatterns that will do .
+                .allowedOriginPatterns("*")
+                //  Whether to allow certificates （cookies）
                 .allowCredentials(true)
+                //  Set allowed methods
+                .allowedMethods("*")
+                //  Cross domain allow time
                 .maxAge(3600);
     }
 }
