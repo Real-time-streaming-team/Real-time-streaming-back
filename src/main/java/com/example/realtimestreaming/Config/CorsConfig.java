f6c6fc17e25b2217d227239b45156e368767abcb
unknown <sola2014@naver.com>
2024-04-28 10:25:41 +0900

    chore: cors 설정 변경

--- src/main/java/com/example/realtimestreaming/Config/CorsConfig.java
+++ src/main/java/com/example/realtimestreaming/Config/CorsConfig.java
@@ -1,3 +1,5 @@
+package com.example.realtimestreaming.Config;
+
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
