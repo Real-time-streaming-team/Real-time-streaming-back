45b408e06a46b5915849840f3d500022f627f462
unknown <sola2014@naver.com>
2024-04-28 10:21:58 +0900

    chore: cors 설정 변경

--- src/main/java/com/example/realtimestreaming/Config/CorsConfig.java
+++ src/main/java/com/example/realtimestreaming/Config/CorsConfig.java
@@ -1,41 +1,28 @@
-package com.example.realtimestreaming.Config;
-
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.Arrays;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
         CorsConfiguration config = new CorsConfiguration();
 
-        // 여러 개의 출처를 허용하도록 설정
-        config.setAllowedOrigins(Arrays.asList(
-                "http://158.247.240.142:3000/",
-                "http://124.46.240.195:63342"
-        ));
-
-        // 허용할 HTTP 메서드 설정
-        config.addAllowedMethod("*");
-        // 허용할 헤더 설정
+        config.setAllowCredentials(false);
+        config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
-        // 인증 정보 허용 설정
-        config.setAllowCredentials(true);
+        config.addAllowedMethod("*");
+        config.setMaxAge(6000L);
 
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+
+        FilterRegistrationBean<CorsFilter> filterBean = new FilterRegistrationBean<>(new CorsFilter(source));
+        filterBean.setOrder(0);
+
+        return filterBean;
     }
 }
--- src/main/java/com/example/realtimestreaming/Config/FirstFilter.java
+++ src/main/java/com/example/realtimestreaming/Config/FirstFilter.java
@@ -1,38 +1,38 @@
-package com.example.realtimestreaming.Config;
-
-import org.springframework.stereotype.Component;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-import java.util.HashSet;
-import java.util.Set;
-
-@Component
-public class FirstFilter implements Filter {
-    private static final Set<String> ALLOWED_ORIGINS = new HashSet<>();
-
-    static {
-        ALLOWED_ORIGINS.add("http://158.247.240.142:3000/");
-        ALLOWED_ORIGINS.add("http://49.142.106.179:3000"); // 허용하려는 다른 도메인 추가
-        ALLOWED_ORIGINS.add("http://124.46.240.195:63342");
-    }
-
-    @Override
-    public void doFilter(ServletRequest request,
-                         ServletResponse response,
-                         FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest req = (HttpServletRequest) request;
-        HttpServletResponse res = (HttpServletResponse) response;
-
-        String origin = req.getHeader("Origin");
-
-        if (ALLOWED_ORIGINS.contains(origin)) {
-            res.addHeader("Access-Control-Allow-Origin", origin);
-        }
-
-        chain.doFilter(req, res);
-    }
-}
+//package com.example.realtimestreaming.Config;
+//
+//import org.springframework.stereotype.Component;
+//
+//import javax.servlet.*;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//
+//import java.util.HashSet;
+//import java.util.Set;
+//
+//@Component
+//public class FirstFilter implements Filter {
+//    private static final Set<String> ALLOWED_ORIGINS = new HashSet<>();
+//
+//    static {
+//        ALLOWED_ORIGINS.add("http://158.247.240.142:3000/");
+//        ALLOWED_ORIGINS.add("http://49.142.106.179:3000"); // 허용하려는 다른 도메인 추가
+//        ALLOWED_ORIGINS.add("http://124.46.240.195:63342");
+//    }
+//
+//    @Override
+//    public void doFilter(ServletRequest request,
+//                         ServletResponse response,
+//                         FilterChain chain) throws IOException, ServletException {
+//        HttpServletRequest req = (HttpServletRequest) request;
+//        HttpServletResponse res = (HttpServletResponse) response;
+//
+//        String origin = req.getHeader("Origin");
+//
+//        if (ALLOWED_ORIGINS.contains(origin)) {
+//            res.addHeader("Access-Control-Allow-Origin", origin);
+//        }
+//
+//        chain.doFilter(req, res);
+//    }
+//}
