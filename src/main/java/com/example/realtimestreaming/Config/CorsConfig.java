0b79cbebdf045bcdfa1a6b82dfd8776fbf99fe7c
unknown <sola2014@naver.com>
2024-05-01 20:49:44 +0900

    chore: 설정 변경

--- src/main/java/com/example/realtimestreaming/Config/CorsConfig.java
+++ src/main/java/com/example/realtimestreaming/Config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
     public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowCredentials(true);
+        config.setAllowCredentials(false);
         config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
