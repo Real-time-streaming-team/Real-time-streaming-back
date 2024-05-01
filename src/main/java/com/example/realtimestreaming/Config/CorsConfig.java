7077c341a37eb1c7dc3b5fca77abf6c481ea4321
unknown <sola2014@naver.com>
2024-05-01 21:04:03 +0900

    chore: 실험

--- src/main/java/com/example/realtimestreaming/Config/CorsConfig.java
+++ src/main/java/com/example/realtimestreaming/Config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(false);
-        config.addAllowedOrigin("*");
+        config.addAllowedOrigin("http://124.46.240.195:63342");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         config.setMaxAge(6000L);
